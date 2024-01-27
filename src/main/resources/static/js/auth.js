2fca72e4a168b0259e1505f77c54dc37837af055
kilokokaina <nikolaushki@gmail.com>
2024-01-27 16:43:11 +0800

    New interface

---
+++ src/main/resources/static/js/auth.js
@@ -0,0 +1,24 @@
+async function login() {
+    let username = document.getElementById('login').value;
+    let password = document.getElementById('password').value;
+
+    let userData = {
+        username: username,
+        password: password
+    };
+
+    console.log(JSON.stringify(userData));
+
+    let response = await fetch('/auth/rest', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json; charset=utf-8'
+        },
+        body: JSON.stringify(userData)
+    });
+
+    let result = response.status;
+    alert(result);
+
+    if (result === 200) location.reload();
+}
